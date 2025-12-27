b1d9c0c825d14c5e4d32a769add2de228728768e
Bryan Penaflor <124495632+BryanPenaflorGD@users.noreply.github.com>
2025-12-27 12:23:26 +0800

    Main Menu

---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SimpleSceneManager : MonoBehaviour
+{
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void ReloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+}
