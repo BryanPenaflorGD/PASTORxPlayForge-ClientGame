b0f61f0a1237ece1b780e57eaab4fcdd7ef08f15
Hadesss00 <162340170+Hadesss00@users.noreply.github.com>
2026-01-05 02:55:29 +0800

    Add SFX, update cutscenes, and implement MusicSFX
    Added new sound effects (SUNNY SFX, WATER PORTAL SFX, CAR_HONK), updated and converted cutscene video files from .mov to .mp4, and updated associated meta files. Modified dialog graphs to trigger new SFX and BGM actions. Updated SelectionScene to include a MusicSFX script and wired up button click events to play sound effects. Also added a Home button UI element.

---
+++ Assets/Scripts/MusicSFX.cs
@@ -0,0 +1,18 @@
+using DialogSystem.Runtime.Core;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicSFX : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioActionHandler.Instance.PlayBGM("Music/BGM_Happy");
+    }
+
+    public void Clicking()
+    {
+        AudioActionHandler.Instance.PlaySFX("SFX/POP");
+    }
+}
